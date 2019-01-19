8d40eefaa195f042d017429bcf82085f682021ac
Stefan Dyulgerov <stefan.dyulgerov@gmail.com>
2019-01-19 13:30:35 +0200

    push file

---
+++ push.bat
@@ -0,0 +1,16 @@
+dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY amd-compressonator-2-3-0.2.3.0.nupkg
+dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY assimp-3-3-1-dll.3.3.1.nupkg
+dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY fbxsdk-2018-1-1-dll.2018.1.1.nupkg
+dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY lzham-uwp-win64.1.0.0.nupkg
+dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY lzham-win64.1.0.0.nupkg
+dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY nvidia-cuda-90-blas.9.0.0.nupkg
+dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY nvidia-cuda-90-cudart.9.0.0.nupkg
+dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY nvidia-cuda-90-cudnn-7.7.2.1.nupkg
+dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY nvidia-cuda-90-fft.9.0.0.nupkg
+dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY nvidia-cuda-90-rand.9.0.0.nupkg
+dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY nvidia-cuda-90-solver.9.0.0.nupkg
+dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY nvidia-cuda-90-sparse.9.0.0.nupkg
+dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY opencv.4.0.0.nupkg
+dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY tensorflow-gpu.1.10.0.nupkg
+dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY tensorflow-lite.1.11.0.nupkg
+              
