cf3d7b4e500376da26d2275efe3e22bac7dd2018
kingofthebongo2008 <stefan.dyulgerov@gmail.com>
2019-08-11 19:05:51 +0300

    fixed

--- push.bat
+++ push.bat
@@ -1,17 +1,21 @@
-dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY amd-compressonator-2-3-0.2.3.1.nupkg
-dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY assimp.4.1.0.nupkg
-dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY assimp-3-3-1-dll.3.3.1.nupkg
-dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY fbxsdk-2018-1-1-dll.2018.1.1.nupkg
-dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY lzham-uwp-win64.1.0.0.nupkg
-dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY lzham-win64.1.0.0.nupkg
-dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY nvidia-cuda-90-blas.9.0.0.nupkg
-dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY nvidia-cuda-90-cudart.9.0.0.nupkg
-dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY nvidia-cuda-90-cudnn-7.7.2.1.nupkg
-dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY nvidia-cuda-90-fft.9.0.0.nupkg
-dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY nvidia-cuda-90-rand.9.0.0.nupkg
-dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY nvidia-cuda-90-solver.9.0.0.nupkg
-dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY nvidia-cuda-90-sparse.9.0.0.nupkg
-dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY opencv.4.0.0.nupkg
-dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY tensorflow-gpu.1.10.0.nupkg
-dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY tensorflow-lite.1.11.0.nupkg
+rem dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY amd-compressonator-2-3-0.2.3.1.nupkg
+rem dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY assimp.4.1.0.nupkg
+rem dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY assimp-3-3-1-dll.3.3.1.nupkg
+rem dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY fbxsdk-2018-1-1-dll.2018.1.1.nupkg
+rem dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY lzham-uwp-win64.1.0.0.nupkg
+rem dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY lzham-win64.1.0.0.nupkg
+rem dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY nvidia-cuda-90-blas.9.0.0.nupkg
+rem dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY nvidia-cuda-90-cudart.9.0.0.nupkg
+rem dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY nvidia-cuda-90-cudnn-7.7.2.1.nupkg
+rem dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY nvidia-cuda-90-fft.9.0.0.nupkg
+rem dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY nvidia-cuda-90-rand.9.0.0.nupkg
+rem dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY nvidia-cuda-90-solver.9.0.0.nupkg
+rem dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY nvidia-cuda-90-sparse.9.0.0.nupkg
+dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY opencv.4.0.2.nupkg
+rem dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY tensorflow-gpu.1.10.0.nupkg
+rem dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY tensorflow-lite.2.0.0.3.nupkg
+dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY khronos-khr.1.0.4.nupkg
+dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY khronos-angle-egl.1.0.0.3.nupkg
+dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY khronos-angle-gles.1.0.0.4.nupkg
+dotnet nuget push -s http://underworld2017.ddns.net:5555/v3/index.json -k NUGET-SERVER-API-KEY tensorflow-lite.2.0.0.7.nupkg
               
